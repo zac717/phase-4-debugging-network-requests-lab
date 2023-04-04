f370ba84be0a3b1be3284be056e6616d14fda6e6
Isaac Ongera <isaacongera717@gmail.com>
2023-04-04 16:55:55 +0300

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,3 +1,4 @@
+
 class ToysController < ApplicationController
   wrap_parameters format: []
 
@@ -7,13 +8,14 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
@@ -23,7 +25,7 @@ class ToysController < ApplicationController
   end
 
   private
-  
+
   def toy_params
     params.permit(:name, :image, :likes)
   end
